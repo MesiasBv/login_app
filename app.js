a0e11ad3bdb6d6dc7a256c83052bd037a07b9068
MesiasBv <barrientosvilcamesias@gmail.com>
2025-06-16 16:48:31 -0500

    Mover los archivos

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use(express.static('public'));
+
 // Rutas
 app.use('/api/auth', authRoutes);
 app.use('/api/getUsers', userRoutes);  // Ruta para obtener usuarios
